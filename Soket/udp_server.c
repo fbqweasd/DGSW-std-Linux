6674ef0b8f3f29a78ed42581b4913ff0f846b288
fbqweasd <fbqweasd2911@gmail.com>
2018-04-05 01:48:03 +0000

    2018_04_05(2)

---
+++ Soket/udp_server.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include<sys/socket.h>
+
+#define UDP_PORT 5100
+
+int main(int agec, char ** argv)
+{
+	int sockfd, n;
+	struct sockaddr_in servaddr, cliaddr;
+	socklen_t len;
+	char mesg[1000];
+	sockfd = socket(PF_INET, SOCK_DGRAM, 0);
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons(UDP_PORT);
+	bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+	do {
+		len = sizeof(cliaddr);
+		n = recvfrom(sockfd, mesg, 1000, 0, (struct sockaddr *)&cliaddr, &len); 
+		strcat(mesg, "from server");
+		sendto(sockfd, mesg, n + 11, 0, (struct sockaddr *)&cliaddr, sizeof(cliaddr));
+		mesg[n]='\0';
+		printf("data : &s\n mesg_len=%d",mesg,strlen(mesg));
+	} while (!strlen(mesg));
+	close(sockfd);
+	return 0;
+}
+
+
